2684aca8c7dffcf5f377a99a3b2b5ed29a878e84
akiiiiiiiiii <45711822+akiiiiiiiiii@users.noreply.github.com>
2021-06-05 14:02:40 +0900

    delete feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,16 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import {
+  READ_EVENTS,
+  DELET_EVENT,
+} from '../actions'
 
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
+    case DELET_EVENT:
+      delete events[action.id]
+      return { ...events }
     default:
       return events
   }
